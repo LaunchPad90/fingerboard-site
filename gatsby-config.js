1939025e987b0bc990d202d565a08754b7cd6bfc
Jacob Hickey <launchpad@Jacobs-MacBook-Pro.local>
2020-12-01 09:04:14 -0700

    netlify update to worky worky

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,16 @@
 require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
 })
+
 module.exports = {
-};
+
+    plugins: [
+      {
+        resolve: `gatsby-plugin-styled-components`,
+        options: {
+          // Add any options here
+        },
+      },
+    ],
+    
+  }
