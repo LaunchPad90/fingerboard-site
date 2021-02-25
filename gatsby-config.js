b2b2e5c1cd2c1175edba5e73a01e9347cf314536
Jacob Hickey <launchpad@Jacobs-MacBook-Pro.local>
2021-02-25 12:38:15 -0700

    metadata update

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,23 +3,27 @@ require("dotenv").config({
 })
 
 module.exports = {
+  siteMetadata: {
+    title: `509 Fingerboards`,
+    siteUrl: `https://www.509fb.netlify.app`,
+    description: `High quality, hand made fingerboards.`,
+  },
 
-    plugins: [
-      {
-        resolve: `gatsby-source-graphql`,
-        options: {
-            typeName: `GraphCMS`,
-            fieldName: `gcms`,
-            url: process.env.GATSBY_GRAPH_CMS_URL,
-            downloadLocalImages: true,
-        },
+  plugins: [
+    {
+      resolve: `gatsby-source-graphql`,
+      options: {
+          typeName: `GraphCMS`,
+          fieldName: `gcms`,
+          url: process.env.GATSBY_GRAPH_CMS_URL,
+          downloadLocalImages: true,
       },
-      {
+    },
+    {
       resolve: `gatsby-plugin-styled-components`,
       options: {
         // Add any options here
       },
     },
-    ],
-    
-  }
+  ],   
+}
